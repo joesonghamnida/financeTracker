9656c38ec8442911075d5baa5cff730b01947669
Joe Morris <joe.morris.dev@gmail.com>
2017-06-18 11:26:08 -0400

    function shells

---
+++ src/main/java/f/sql/SQL.java
@@ -0,0 +1,47 @@
+package f.sql;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class SQL {
+
+    public static Connection connection() throws SQLException {
+        Connection conn = DriverManager.getConnection("jdbc:h2:./main");
+        return conn;
+    }
+
+    //tables to be created: bank, account type (checking or savings), credit cards, loans
+
+    //TODO: flesh out db
+    public static void createDatabase(Connection conn)throws SQLException{
+        Statement statement = conn.createStatement();
+        statement.execute("CREATE TABLE IF NOT EXISTS finances(id IDENTITY)");
+    }
+
+    public static void createBanksTable(Connection conn)throws SQLException{
+
+    }
+
+    public static void createAccountsTable(Connection conn)throws SQLException{
+
+    }
+
+    public static void createCreditTable(Connection conn)throws SQLException{
+
+    }
+
+    public static void createLoansTable(Connection conn)throws SQLException{
+
+    }
+
+    public static void insertRecord(){}
+
+    public static void updateRecord(){}
+
+    public static void retrieveRecord(){}
+
+    public static void deleteRecord(){}
+
+}
